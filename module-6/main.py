40c7b1060e1b111d797ed31e21efaa4d67f80bab
Luke Fitzgerald <luke.fitzgerald@nutrien.com>
2025-04-25 16:47:53 -0400

    making updates after more testing

--- module-6/main.py
+++ module-6/main.py
@@ -1,18 +1,3 @@
-'''
-In the main section of your code, implement the print_menu() function. print_menu() has a ShoppingCart parameter and outputs a menu of options to manipulate the shopping cart. Each option is represented by a single character. Build and output the menu within the function.
-
-If an invalid character is entered, continue to prompt for a valid choice. Hint: Implement Quit before implementing other options. Call print_menu() in the main() function. Continue to execute the menu until the user enters q to Quit.
-Example:
-MENU
-a - Add item to cart
-r - Remove item from cart
-c - Change item quantity
-i - Output items' descriptions
-o - Output shopping cart
-q - Quit
-Choose an option:
-'''
-
 from order_class import ItemToPurchase
 from shopping_cart import ShoppingCart
 
@@ -57,11 +42,14 @@ while(user_input != "q"):
         item = cart.get_item(item_name)
 
         if item != None:
+            print("Enter updated quantity:")
+            new_quantity = int(input())
+            item.quantity = new_quantity
             cart.modify_item(item)
     elif(user_input == "i"):
         cart.print_descriptions()
     elif(user_input == "o"):
         cart.print_total()
     
-    print("Choose an option:")
+    print_menu(cart)
     user_input = str(input())
--- module-6/shopping_cart.py
+++ module-6/shopping_cart.py
@@ -1,67 +1,3 @@
-'''
-Build the ShoppingCart class with the following data attributes and related methods. Note: Some can be method stubs (empty methods) initially, to be completed in later steps
-
-Parameterized constructor, which takes the customer name and date as parameters
-Attributes
-customer_name (string) - Initialized in default constructor to "none"
-current_date (string) - Initialized in default constructor to "January 1, 2020"
-cart_items (list)
-Methods
-add_item()
-Adds an item to cart_items list. Has parameter ItemToPurchase. Does not return anything.
-remove_item()
-Removes item from cart_items list. Has a string (an item's name) parameter. Does not return anything.
-If item name cannot be found, output this message: Item not found in cart. Nothing removed.
-modify_item()
-Modifies an item's description, price, and/or quantity. Has parameter ItemToPurchase. Does not return anything.
-If item can be found (by name) in cart, check if parameter has default values for description, price, and quantity. If not, modify item in cart.
-If item cannot be found (by name) in cart, output this message: Item not found in cart. Nothing modified.
-get_num_items_in_cart()
-Returns quantity of all items in cart. Has no parameters.
-get_cost_of_cart()
-Determines and returns the total cost of items in cart. Has no parameters.
-print_total()
-Outputs total of objects in cart.
-If cart is empty, output this message: SHOPPING CART IS EMPTY
-print_descriptions()
-Outputs each item's description.
-Example of print_total() output:
-John Doe's Shopping Cart - February 1, 2020
-Number of Items: 8
-Nike Romaleos 2 @ $189 = $378
-Chocolate Chips 5 @ $3 = $15
-Powerbeats 2 Headphones 1 @ $128 = $128
-Total: $521
-
-Example of print_descriptions() output:
-John Doe's Shopping Cart - February 1, 2020
-Item Descriptions
-Nike Romaleos: Volt color, Weightlifting shoes
-Chocolate Chips: Semi-sweet
-Powerbeats 2 Headphones: Bluetooth headphones
-
-
-Implement Output shopping cart menu option. Implement Output item's description menu option.
-
-Example of shopping cart menu option:
-OUTPUT SHOPPING CART
-John Doe's Shopping Cart - February 1, 2020
-Number of Items: 8
-Nike Romaleos 2 @ $189 = $378
-Chocolate Chips 5 @ $3 = $15
-Powerbeats 2 Headphones 1 @ $128 = $128
-Total: $521
-
-Example of item description menu option.
-OUTPUT ITEMS' DESCRIPTIONS
-John Doe's Shopping Cart - February 1, 2020
-Item Descriptions
-Nike Romaleos: Volt color, Weightlifting shoes
-Chocolate Chips: Semi-sweet
-Powerbeats 2 Headphones: Bluetooth headphones
-
-'''
-
 class ShoppingCart:
     def __init__(self, customer_name = "none", date = "January 1, 2020"):
         self.customer_name = customer_name
@@ -83,27 +19,31 @@ class ShoppingCart:
     def remove_item(self, item_name):
         # Removes item from cart_items list. Has a string (an item's name) parameter. Does not return anything.
         # If item name cannot be found, output this message: Item not found in cart. Nothing removed.
+        item_removed = False
         for item in self.cart_items:
             if item.name == item_name:
                 self.cart_items.remove(item)
+                item_removed = True
                 break
-            else:
-                print("Item not found in cart. Nothing removed.")
+        if item_removed == False:
+            print("Item not found in cart. Nothing removed.")
 
     def modify_item(self, ItemToPurchase):
         # Modifies an item's description, price, and/or quantity. Has parameter ItemToPurchase. Does not return anything.
         # If item can be found (by name) in cart, check if parameter has default values for description, price, and quantity. If not, modify item in cart.
         # If item cannot be found (by name) in cart, output this message: Item not found in cart. Nothing modified.
+        item_found = False
         for item in self.cart_items:
             if item.name == ItemToPurchase.name:
+                item_found = True
                 if ItemToPurchase.description != "none":
                     item.description = ItemToPurchase.description
                 if ItemToPurchase.price != 0:
                     item.price = ItemToPurchase.price
                 if ItemToPurchase.quantity != 0:
                     item.quantity = ItemToPurchase.quantity
-            else:
-                print("Item not found in cart. Nothing modified.")
+        if item_found == False:
+            print("Item not found in cart. Nothing modified.")
 
     def get_num_items_in_cart(self):
         # Returns quantity of all items in cart. Has no parameters.
@@ -119,17 +59,20 @@ class ShoppingCart:
     def print_total(self):
         # Outputs total of objects in cart.
         # If cart is empty, output this message: SHOPPING CART IS EMPTY
+        print("OUTPUT SHOPPING CART")
         if len(self.cart_items) == 0:
             print("SHOPPING CART IS EMPTY")
         else:
             print("{}'s Shopping Cart - {}".format(self.customer_name, self.date))
             print("Number of Items: {}".format(self.get_num_items_in_cart()))
             for item in self.cart_items:
-                print("{} {} @ ${:.2f} = ${:.2f}".format(item.name, item.quantity, item.price, item.price * item.quantity))
-            print("Total: ${}".format(self.get_cost_of_cart()))
+                # print("{} {} @ ${:.2f} = ${:.2f}".format(item.name, item.quantity, item.price, item.price * item.quantity))
+                item.print_item_cost()
+            print("Total: ${:.2f}".format(self.get_cost_of_cart()))
 
     def print_descriptions(self):
         # Outputs each item's description.
+        print("OUTPUT ITEMS' DESCRIPTIONS")
         if len(self.cart_items) == 0:
             print("SHOPPING CART IS EMPTY")
         else:
